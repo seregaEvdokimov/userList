02940ae4e8c52da1a93aa7434fce419587f0042e
Evdokimov <s.evdokimov@itransition.com>
2016-11-30 18:13:51 +0300

    fix worker

--- js/worker.js
+++ js/worker.js
@@ -5,31 +5,37 @@
 importScripts('https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.6.0/socket.io.js');
 var socket = io('http://localhost:4000/');
 var connections = 0; // count active connections
-var port;
+var ports = [];
 
 self.addEventListener("connect", function (e) {
-    port = e.ports[0];
-    connections++;
+    var port = e.ports[0];
+    ports.push(port);
+    port.start();
+    // connections++;
 
     port.addEventListener("message", function (e) {
         // port.postMessage(e.data);
     }, false);
-
-    port.start();
 }, false);
 
 
 socket.on('connect', function (res) {
     var data = JSON.stringify({type: 'connect', payload: res});
-    port.postMessage(data);
+    ports.forEach(function(item) {
+        item.postMessage(data);
+    });
 });
 
 socket.on('new user', function (res) {
     var data = JSON.stringify({type: 'new user', payload: res});
-    port.postMessage(data);
+    ports.forEach(function(item) {
+        item.postMessage(data);
+    });
 });
 
 socket.on('time passed', function(res){
     var data = JSON.stringify({type: 'time passed', payload: res});
-    port.postMessage(data);
+    ports.forEach(function(item) {
+        item.postMessage(data);
+    });
 });
