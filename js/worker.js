667ef72e66e22114909592be0adb278e3cdd216c
Evdokimov <s.evdokimov@itransition.com>
2016-11-30 17:52:28 +0300

    fix;

--- js/worker.js
+++ js/worker.js
@@ -5,33 +5,31 @@
 importScripts('https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.6.0/socket.io.js');
 var socket = io('http://localhost:4000/');
 var connections = 0; // count active connections
+var port;
 
 self.addEventListener("connect", function (e) {
-    var port = e.ports[0];
+    port = e.ports[0];
     connections++;
 
-    // port.addEventListener("message", function (e) {
+    port.addEventListener("message", function (e) {
         // port.postMessage(e.data);
-    // }, false);
-
-    socket.on('connect', function (res) {
-        var data = JSON.stringify({type: 'connect', payload: res});
-        port.postMessage(data);
-    });
-
-    socket.on('new user', function (res) {
-        var data = JSON.stringify({type: 'new user', payload: res});
-        port.postMessage(data);
-    });
-
-    socket.on('time passed', function(res){
-        var data = JSON.stringify({type: 'time passed', payload: res});
-        port.postMessage(data);
-    });
+    }, false);
 
     port.start();
 }, false);
 
 
+socket.on('connect', function (res) {
+    var data = JSON.stringify({type: 'connect', payload: res});
+    port.postMessage(data);
+});
 
+socket.on('new user', function (res) {
+    var data = JSON.stringify({type: 'new user', payload: res});
+    port.postMessage(data);
+});
 
+socket.on('time passed', function(res){
+    var data = JSON.stringify({type: 'time passed', payload: res});
+    port.postMessage(data);
+});
